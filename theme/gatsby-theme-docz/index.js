d2d772e34d54bf3ebd5bd342026816146262a9d1
rakannimer <rakannimer@gmail.com>
2019-11-05 08:28:19 +0200

    fix: import ThemeProvider from theme-ui

--- theme/gatsby-theme-docz/index.js
+++ theme/gatsby-theme-docz/index.js
@@ -1,29 +1,25 @@
-import React from 'react'
-import { theme, useConfig, ComponentsProvider } from 'docz'
-import { ThemeProvider } from 'emotion-theming'
-import baseComponents from 'gatsby-theme-docz/src/components'
-
-const componentsMap = {
-  ...baseComponents
-}
+import React from "react";
+import { theme, useConfig, ComponentsProvider } from "docz";
+import { ThemeProvider } from "theme-ui";
+import baseComponents from "gatsby-theme-docz/src/components";
 
 const Theme = ({ children }) => {
-  const config = useConfig()
+  const config = useConfig();
   return (
     <ThemeProvider theme={config}>
       <ComponentsProvider components={baseComponents}>
         {children}
       </ComponentsProvider>
     </ThemeProvider>
-  )
-}
+  );
+};
 
 const themeConfig = {
   colors: {
-    primary: 'tomato',
-    secondary: 'khaki',
-    gray: 'lightslategray',
-  },
-}
+    primary: "tomato",
+    secondary: "khaki",
+    gray: "lightslategray"
+  }
+};
 
-export default theme(themeConfig)(Theme)
+export default theme(themeConfig)(Theme);
